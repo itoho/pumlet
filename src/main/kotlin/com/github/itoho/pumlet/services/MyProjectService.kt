1326bd52c56bd3750d580337491a401191235f22
GitHub Action <action@github.com>
2021-09-17 02:43:35 +0000

    Template cleanup

--- src/main/kotlin/com/github/itoho/pumlet/services/MyProjectService.kt
+++ src/main/kotlin/com/github/itoho/pumlet/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.itoho.pumlet.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.itoho.pumlet.MyBundle
 
 class MyProjectService(project: Project) {
 
